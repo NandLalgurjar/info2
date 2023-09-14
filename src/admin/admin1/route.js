5ab05a89b0422a787ea3869ede7d31de5dcffb3b
NandLalgurjar <nlgurjar.img@gmail.com>
2023-09-14 10:51:26 +0530

    auth

--- src/admin/admin1/route.js
+++ src/admin/admin1/route.js
@@ -1,4 +1,4 @@
-const auth = require("../../middleware/adminauth")
+// const auth = require("../../middleware/adminauth");
 const Upload = require("../../middleware/img");
 const responseHandler = require("../../utils/responseHandlers");
 const { Router } = require("express");
@@ -14,14 +14,14 @@ app.post("/register-admin-data", adminRegisterData);
 app.get("/login", login);
 app.post("/login-admin-data", loginData);
 
-app.get("/forget-password", auth, forgetPassword)
-app.post("/Forget-password", auth, ForgetPassword)
-app.get("/users-profile", auth, usersProfile);
-app.post("/add_profile_data", auth, Upload.single("image"), add_profile_data)
+app.get("/forget-password", forgetPassword)
+app.post("/Forget-password", ForgetPassword)
+app.get("/users-profile", usersProfile);
+app.post("/add_profile_data", Upload.single("image"), add_profile_data)
 
 app.get("/Admin-log-out", AdminlogOut)
 
 // ajex
-app.get("/payment-Revenues", auth, paymentRevenues)
+app.get("/payment-Revenues", paymentRevenues)
 
 module.exports = app
