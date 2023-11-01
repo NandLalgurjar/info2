047ed8d9834c8108ed7ad1cb0a50fc274b3a556e
NandLalgurjar <nlgurjar.img@gmail.com>
2023-11-01 06:55:03 +0530

    resoleved

--- src/admin/admin1/controller.js
+++ src/admin/admin1/controller.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const userModel = require("../../api/user/model");
-const { findByIdAndUpdate } = require('./model');
 const path = require("path")
 const saloon = require("../../api/saloonstore/model")
 
@@ -10,33 +9,34 @@ const service = require("./service")
 
 exports.admin = async (req, res) => {
     try {
-        if (req.cookies.accessToken) {
-            const { _id } = jwt.verify(req.cookies.accessToken, process.env.accessToken)
-            const user = await userModel.findOne({ _id })
-            res.locals.message = req.flash();
+        res.locals.message = req.flash();
+        if (req.cookies?.accessToken) {
+            const { _id } = jwt.verify(req.cookies.accessToken, process.env.accessToken);
+            const user = await userModel.findOne({ _id });
             if (user) {
-                req.user = user
-                let data = await service.AllDetail(req)
-                res.render("users/dashboard", { user, data })
+                req.user = user;
+                let data = { order: 10, user: 10, saloon: 10, FindRequist: 10, Findservice: 10, Findpackeges: 10 }
+                req.flash("success", "welcome back to Dashboard");
+                res.render("users/dashboard", { user, data, title: "Dashboard" });
             } else {
-                res.locals.message = req.flash();
-                res.render("users/login")
-            }
+                res.render("users/login");
+            };
         } else {
-            res.locals.message = req.flash();
-            res.render("users/login")
-        }
+            res.render("users/login");
+        };
     } catch (error) {
-        console.log(error);
-    }
-}
+        req.flash("error", error.message);
+        res.redirect("/login");
+    };
+};
 
 exports.register = async (req, res) => {
     try {
         res.locals.message = req.flash();
         res.render("users/register");
     } catch (error) {
-        console.log(error);
+        req.flash("error", error.message);
+        res.redirect("/login");
     };
 };
 
@@ -81,48 +81,44 @@ exports.loginData = async (req, res) => {
     try {
         res.locals.message = req.flash();
         const { email, password } = req.body;
-        if (email) {
-            const user = await userModel.findOne({ email: req.body.email });
-            if (user) {
-                if (typeof user.password === 'undefined') {
-                    req.flash("error", "Detail is Not Found ");
-                    return res.redirect("/");
-                };
-                if (user.type != "admin" && user.type != "super-admin") {
-                    req.flash("error", "your are not eligible to login");
-                    return res.redirect("/");
-                };
-                const match = await bcrypt.compare(password, user.password);
-                if (match) {
-                    const accessToken = jwt.sign({ _id: user._id }, process.env.accessToken);
-                    const refreshToken = jwt.sign({ _id: user._id }, process.env.refreshToken);
-
-                    res.cookie("accessToken", accessToken, {
-                        expires: new Date(Date.now() + 10000 * 60 * 60),//1 minit
-                        httpOnly: true,
-                        overwrite: true
-                    }).cookie("refreshToken", refreshToken, {
-                        expires: new Date(Date.now() + 10000 * 60 * 60 * 12),//10 minit
-                        httpOnly: true,
-                        overwrite: true
-                    });
-                    req.flash("success", "login successfully");
-                    return res.redirect("/");
-                } else {
-                    req.flash("error", "invalid login details");
-                    return res.redirect("/");
-                };
-            } else {
-                req.flash("error", "invalid login details");
-                return res.redirect("/");
-            };
+        if (!email || !password) {
+            req.flash("error", "invalid login details");
+            return res.redirect("/");
         };
+
+        const user = await userModel.findOne({ email: req.body.email });
+        if (!user) {
+            req.flash("error", "wroung email/password details");
+            return res.redirect("/");
+        };
+
+        if (user.type != "admin" && user.type != "super-admin") {
+            req.flash("error", "your are not eligible to login");
+            return res.redirect("/");
+        };
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            req.flash("error", "invalid login details");
+            return res.redirect("/");
+        }
+        const accessToken = jwt.sign({ _id: user._id }, process.env.accessToken);
+        res.cookie("accessToken", accessToken, {
+            expires: new Date(Date.now() + 10000 * 60 * 60),//1 minit
+            httpOnly: true,
+            overwrite: true
+        });
+        req.flash("success", "login successfully");
+        return res.redirect("/");
     } catch (error) {
-        console.log(error);
+        req.flash("error", error.message);
+        return res.redirect("/");
     };
 };
+
 exports.forgetPassword = async (req, res) => {
     try {
+        console.log(req.user, "=req.user")
+        res.locals.message = req.flash();
         res.render("users/Forget-Password", { user: req.user })
     } catch (error) {
         console.log(error);
@@ -197,20 +193,21 @@ exports.add_profile_data = async (req, res) => {
 
 exports.AdminlogOut = async (req, res) => {
     try {
-        res.clearCookie("accessToken", 'token', { expires: new Date(0) })
-            .clearCookie("refreshToken", 'token', { expires: new Date(0) })
-            .redirect("/");
+        res.locals.message = req.flash();
+        req.flash("success", "log out successfully");
+        res.clearCookie("accessToken", 'token', { expires: new Date(0) }).redirect("/");
     } catch (error) {
+        req.flash("error", error.message);
         console.log(error);
-    }
-}
+    };
+};
 
 const payment = require("../../api/payment/model")
 exports.paymentRevenues = async (req, res) => {
     try {
         let obj = {};
         let data;
-        if (req.user.type == "admin") {
+        if (req.user?.type == "admin") {
             let condition = [];
             condition.push({
                 '$match': {
